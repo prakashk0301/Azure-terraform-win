fcb1a72c2436247e5e74d6b082e9d359328bbfef
prakashk0301 <34745120+prakashk0301@users.noreply.github.com>
2020-05-12 01:35:49 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+subscription_id = "XXXXXXXXXXXXXXXXXXXXXXXXXX"
+client_id = "XXXXXXXXXXXXXXXXXXXXXXXXXX"
+client_secret = "XXXXXXXXXXXXXXXXXXXXXXXXXX"
+tenant_id = "XXXXXXXXXXXXXXXXXXXXXXXXXX"
+features {}
+}
